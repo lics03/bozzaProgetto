5877ee0f24a07ecc8edb6f52a8e4f562731a2ee1
Jessica <jessica_brandi_@hotmail.it>
2019-03-28 23:38:46 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  #get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get 'static_pages/about'  
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -9,7 +11,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'users#index'
+  #root 'users#index'
+
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
