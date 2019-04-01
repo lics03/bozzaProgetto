43fd89b774a5e5f6300ff4c1412ef7624611e06e
Jessica <jessica_brandi_@hotmail.it>
2019-04-01 19:30:02 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   #get 'static_pages/home'
   
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
-  resources :users
 
   get 'signup'  => 'users#new'
 
